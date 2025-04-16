bb2126dd40a506aa90729355c5d26221165ab2d5
lianglei <stone.ll@qq.com>
2025-04-16 17:49:41 +0800

    feat:多入口打包工具函数配置

---
+++ build/utils.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const glob = require('glob');
+
+function resolve(...dirs) {
+  return path.join(__dirname, '../', ...dirs);
+}
+
+function normalize(parent, ...dirname) {
+  const dir = path.join(parent, ...dirname);
+  const reg = /\\/g;
+
+  return reg.test(dir) ? dir.replace(reg, '/') : dir;
+}
+
+const componentDirs = '{form,table}';
+
+function getResloveMap(prefix = '', dirname = componentDirs) {
+  const resloveMap = {};
+  const dirs = glob.sync(`${prefix}/packages/${dirname}/*`);
+
+  dirs.forEach((dir) => {
+    const val = prefix ? i.replace(prefix, '') : i;
+    const key = val.split('/').reverse()[0];
+    resloveMap[key] = resolve(val);
+  });
+
+  return resloveMap;
+}
+
+const componentExternals = {};
+
+function getEntryAndExternals(dirname = componentDirs) {
+  const entry = {};
+  glob.sync(`packages/${dirname}/**/*.{js,vue}`)
+    .map(i => {
+      const val = i;
+      const key = val.replace('packages/', '').replace(/(\/index)?\.(js|vue)/, '');
+      if (!key.endsWith('.umd')) {
+        componentExternals[key] = `ll-form-table/${key}`;
+        entry[i.replace('packages/', '').replace(/\.(js|vue)/, '')] = `./${val}`;
+      }
+    });
+
+  entry['index'] = './packages/index.js';
+  entry['externals'] = './packages/externals.js';
+
+  return {
+    entry,
+    componentExternals,
+  };
+}
+
+module.exports = {
+  resolve,
+  normalize,
+  getResloveMap,
+  getEntryAndExternals
+};
---
+++ src/packages/externals.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'll-form-table': {
+    commonjs: 'll-form-table',
+    commonjs2: 'll-form-table',
+    root: 'LlFormTable'
+  },
+  'll-form-table/form': {
+    commonjs: 'll-form-table/form',
+    commonjs2: 'll-form-table/form',
+    root: ['LlFormTable', 'Form']
+  },
+  'll-form-table/table': {
+    commonjs: 'll-form-table/table',
+    commonjs2: 'll-form-table/table',
+    root: ['LlFormTable', 'Table']
+  },
+};
